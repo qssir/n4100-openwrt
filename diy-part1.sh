bb0517f5e48aff50e12d0f66fbfbb6aa82021a90
qssir <97458294+qssir@users.noreply.github.com>
2026-06-27 21:02:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,5 +10,8 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# 安全注入 DAED 第三方高性能代理软件源，规避 Git 冲突机制
+# 1. 修复 Actions 虚拟主机创建 swapfile 时的权限被拒问题
+sudo chmod 777 / 2>/dev/null || true
+
+# 2. 安全注入 DAED 第三方高性能代理软件源，规避 Git 冲突
 echo 'src-git daeuniverse https://github.com/daeuniverse/openwrt-packages.git;main' >> feeds.conf.default
